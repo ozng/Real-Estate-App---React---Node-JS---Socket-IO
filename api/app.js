b5bb1235ba477388a3195f6b5cfa55c88f642567
Ozan G <ozangurer12@gmail.com>
2024-06-11 18:55:34 +0300

    login controller -> generating token & cookie

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/post", postRoute);
 app.use("/api/auth", authRoute);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -21,5 +22,45 @@ export const register = async (req, res) => {
     res.status(500).json({ message: "Failed to create a user." });
   }
 };
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
+
+    const experationDateSevenDay = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: experationDateSevenDay }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure: true, - TODO - uncomment on production
+        maxAge: experationDateSevenDay,
+      })
+      .status(200)
+      .json({ message: "successfully logged in" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Failed to login." });
+  }
+};
 export const logout = (req, res) => {};
