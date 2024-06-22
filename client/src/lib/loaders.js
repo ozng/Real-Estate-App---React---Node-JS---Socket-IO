1182510845bbabe0c88cb7e957f665e75f7b7548
Ozan G <ozangurer12@gmail.com>
2024-06-22 13:35:30 +0300

    list page fetch query results

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -1,7 +1,14 @@
 import apiRequest from "./apiRequest";
 
-export const singlePageLoader = async ({ _request, params }) => {
+export const singlePageLoader = async ({ request, params }) => {
   const response = await apiRequest("/post/" + params.id);
+
+  return response.data;
+};
+
+export const listPageLoader = async ({ request, params }) => {
+  const query = request.url.split("?")[1];
+  const response = await apiRequest("/post?" + query);
 
   return response.data;
 };
