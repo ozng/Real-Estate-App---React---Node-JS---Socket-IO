fee1863889e1a74ff74379c6cdb432337d0445dd
Ozan G <ozangurer12@gmail.com>
2024-06-26 15:59:40 +0300

    filtering in list page, fixing card image

---
+++ client/src/components/filter/useFilter.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const useFilter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || 0,
+    maxPrice: searchParams.get("maxPrice") || 100000000,
+    bedroom: searchParams.get("bedroom") || 1,
+  });
+
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
+
+  return {
+    handleChange,
+    handleFilter,
+    query,
+    searchParams,
+  };
+};
+
+export default useFilter;
